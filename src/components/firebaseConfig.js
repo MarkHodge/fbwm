7f09e1e701dd9898cfa27a15df90c6d65b35e3a3
Mark Hodge <mark.978@email.com>
2019-05-27 16:54:40 -0500

    Added Firebase

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBa3Fx7EIEGpI9GDRdZFbgogdblWsb22X0",
+    authDomain: "fbwm-2ef2a.firebaseapp.com",
+    databaseURL: "https://fbwm-2ef2a.firebaseio.com",
+    projectId: "fbwm-2ef2a",
+    storageBucket: "fbwm-2ef2a.appspot.com",
+    messagingSenderId: "551352635745",
+    appId: "1:551352635745:web:971b4bc26d0256a2"
+};
